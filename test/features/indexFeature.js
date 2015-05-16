eb58b5840cb2e8f95f41a1dd9499aa2d7a562638
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-05-16 20:12:44 +0100

    Refactored tests and all working

--- test/features/indexFeature.js
+++ test/features/indexFeature.js
@@ -14,24 +14,9 @@ describe('Homepage', function() {
     client.end(done)
   });
 
-  function createOneUser() {
-    client
-      .url('http://localhost:3001/index')
-      .setValue('#name', 'Clint')
-      .click('#join')
-    return client;
-  };
-
-  function createTwoUsers() {
-    createOneUser('Clint')
-      .newWindow('http://localhost:3001/index', 'Chat 2')
-      .setValue('#name', 'Jake')
-      .click('#join')
-    return client;
-  };
-
   it('should let a user know when a message has been sent', function(done){
-   createOneUser('Clint')
+    client
+    .url('http://localhost:3001')
     .setValue('#msg', 'Hi from Clint')
     .click('#send')
     .getText('#msgs', function(err, text){
@@ -41,54 +26,30 @@ describe('Homepage', function() {
   });
 
   it('should let a user know who is talking', function(done) {
-    createOneUser('Clint')
-    .setValue('#msg', 'Hi from Clint')
+    client
+    .url('http://localhost:3001')
     .click('#send')
     .getText('#msgs', function(err, text){
-      expect(text).to.contain('Clint:')
+      expect(text).to.contain('Tom:')
     })
     .call(done);
   });
 
   it('should tell user when it has connected', function(done) {
-    createOneUser('Clint')
+    client
+      .url('http://localhost:3001')
       .getText('#msgs', function(err, text){
         expect(text).to.contain('You have connected to the server.')
       })
       .call(done);
   });
 
-  it('should let a user know when someone has disconnected from the server', function(done){
-    createTwoUsers()
-      .close()
-      .getText('#msgs', function(err, text){
-        expect(text).to.contain('Jake has left the server')
-      })
-      .call(done);
-  });
-
   it('should let users know when a new user joins', function(done) {
-    createTwoUsers()
+    client
+      .url('http://localhost:3001')
       .switchTab()
       .getText('#msgs', function(err, text) {
-        expect(text).to.contain('Jake has joined the server.')
-      })
-      .call(done);
-  });
-
-  it('should update node user list when a new user joins', function(done) {
-    createTwoUsers()
-      .getText('#users', function(err, text) {
-        expect(text).to.contain('Jake')
-      })
-      .call(done);
-  });
-
-  it('should update node user list when someone leaves', function(done) {
-    createTwoUsers()
-      .close()
-      .getText('#users', function(err, text) {
-        expect(text).not.to.contain('Jake')
+        expect(text).to.contain('You have connected to the server.')
       })
       .call(done);
   });
