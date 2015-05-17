36a6365796444bc687c39acb13bb31ac5658c9ee
Ivysaur <contact@pchennz.com>
2015-05-17 10:43:34 +0100

    combining approve/disapprove/extend into single socket handler

--- zerver/libs/socket-handlers/_trader-trade-response.js
+++ zerver/libs/socket-handlers/_trader-trade-response.js
@@ -2,7 +2,7 @@ module.exports = function(people, client) {
 
   client.on('trader-make-approve', function(user) {
     //TODO
-    // 1. broadcast trade approved
+    // 1. if paired, broadcast (actually only send to pair)
   });
 
 };
